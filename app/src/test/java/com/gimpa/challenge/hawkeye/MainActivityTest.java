34dccb5c1baae2a0f35efaca628ec8ea6190e16e
Ezekiel Sebastine <ezekiel@farmerline.co>
2020-02-12 00:03:20 +0000

    refactor

---
+++ app/src/test/java/com/gimpa/challenge/hawkeye/MainActivityTest.java
@@ -0,0 +1,20 @@
+package com.gimpa.challenge.hawkeye;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Test
+    public void getPath() {
+    }
+
+    @Test
+    public void processFinish() {
+    }
+
+    @Test
+    public void startAnim() {
+    }
+}
